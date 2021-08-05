d40c8cb28d276f708fd6bca25fa1b9063103e759
verobragina <verobragina@ya.ru>
2021-08-05 18:16:02 +0300

    Imptement lifecycle methods

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,28 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
-import ItemList from '../item-list';
-import PersonDetails from '../person-details';
+import ErrorIndicator from '../error-indicator';
+import PeoplePage from '../people-page';
 
 import './app.css';
 
-const App = () => {
-  return (
-    <div>
-      <Header />
-      <RandomPlanet />
+export default class App extends Component {
+  state = {
+    showRandomPlanet: true,
+    hasError: false
+  }
 
-      <div className="row mb2">
-        <div className="col-md-6">
-          <ItemList />
-        </div>
-        <div className="col-md-6">
-          <PersonDetails />
-        </div>
+  componentDidCatch() {
+    this.setState({hasError: true})
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <ErrorIndicator />
+    }
+
+    return (
+      <div>
+        <Header/>
+        <RandomPlanet/>
+        <PeoplePage />
       </div>
-    </div>
-  );
-};
-
-export default App;
+    )
+  }
+}
